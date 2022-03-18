0a3d08f71f08e28e6f9077ca87b777eb073f0c13
DustinRepo <tinmann4300@gmail.com>
2022-03-17 20:27:44 -0400

    Renamed to MinecraftAssets and changed to dump all assets

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Lang Extractor
-A simple java program designed to automatically grab all Minecraft lang files 
+A simple java program designed to automatically grab and collect all Minecraft asset files
 
 ## How to use
 ```
 1. Run Minecraft for the specified version atleast once
 2. Open file explorer and navigate to ".minecraft/assets/indexes"
 3. Verify the index file exists, even if it is for a slightly different version (e.g, 1.12.json comes from 1.12.2 MC)
-4. Run the jar in terminal with the specified version. "java -jar LangExtractor.jar 1.18"
-5. Output files should be in a folder called "lang_(version)"
+4. Run the jar in terminal with the specified version. "java -jar MinecraftAssets.jar --version=1.18"
+5. Output files will be in a folder named after the version specified.
 ```
+
+## For custom Minecraft directories
+If you don't have minecraft in your normal location, you can specify it using --mcDir=/path/to/.minecraft For example:
+`java -jar --version=1.18 --mcDir=/home/user/.minecraft`
+
+## What does this do?
+Reads the MC assets index file for the specified version, that tell us what the file is named and where it is (based on the hash),
+After that, it automatically finds each file then copies it in it's original directory structure to readily usable files
---
+++ src/me/dustin/langextractor/MinecraftAssets.java
@@ -0,0 +1,78 @@
+package me.dustin.langextractor;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import me.dustin.langextractor.util.Util;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+
+public class MinecraftAssets {
+
+    public static final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+
+    public static void main(String[] args) {
+        String version = null;
+        String customMcDir = null;
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("--help")) {
+                System.out.println("--version=<v>   |   Sets the version to get the assets from");
+                System.out.println("--mcDir=<path>  |   Sets the directory for .minecraft folder (optional)");
+
+                System.out.println("\nPlease read the README file on the github if you need more information");
+                System.out.println("Example: java -jar MinecraftAssets.jar --version=1.12");
+                return;
+            } else if (arg.startsWith("--version=")) {
+                version = arg.split("=")[1];
+            } else if (arg.startsWith("--mcDir=")) {
+                customMcDir = arg.split("=")[1];
+            }
+        }
+        if (version == null) {
+            System.out.println("Error! have to specify a version");
+            return;
+        }
+
+        File mcDir = customMcDir == null ? Util.getMCDir() : new File(customMcDir);
+        if (!mcDir.exists()) {
+            System.out.println("Error! No Minecraft found!");
+            return;
+        }
+        File assetDir = new File(mcDir, "assets");
+        File indexesDir = new File(assetDir, "indexes");
+        File objectsDir = new File(assetDir, "objects");
+
+        File indexJson = new File(indexesDir, version + ".json");
+
+        if (!indexJson.exists()) {
+            System.out.println("Error! Index file not found at: " + indexJson.getAbsolutePath());
+            return;
+        }
+
+        File outputFolder = new File(new File("").getAbsolutePath(), version.replace(".", "_"));
+
+        String s = Util.readFile(indexJson);
+        JsonObject jsonObject = gson.fromJson(s, JsonObject.class);
+        Set<Map.Entry<String, JsonElement>> entrySet = jsonObject.get("objects").getAsJsonObject().entrySet();
+        for(Map.Entry<String,JsonElement> entry : entrySet) {
+            JsonObject o = entry.getValue().getAsJsonObject();
+            String hash = o.get("hash").getAsString();
+            File containingFolder = new File(objectsDir, hash.substring(0, 2));
+            File langFile = new File(containingFolder, hash);
+            File outputFile = new File(outputFolder, entry.getKey());
+            try {
+                if (!outputFile.getParentFile().exists())
+                    outputFile.getParentFile().mkdirs();
+                Util.copyFile(langFile, outputFile);
+                System.out.println("Copied file " + entry.getKey());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Finished...");
+    }
+
+}
